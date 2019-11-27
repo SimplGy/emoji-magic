21349ea87d4612cb9b868a48d855d3def8ee2c31
Eric Miller <hi@simple.gy>
2019-11-26 23:35:00 -0800

    bump version, limit to 15 rows of icons

--- src/browser_action/emoji.js
+++ src/browser_action/emoji.js
@@ -18,7 +18,7 @@ const store = require('./store');
 
 module.exports = (() => {
   const RECENT_KEY = 'recent-selections';
-  const RESULT_LIMIT = 8 * 20; // for render perf, don't draw everything
+  const RESULT_LIMIT = 8 * 15; // for render perf, don't draw everything. 15 rows fit in Chrome's 600px height limit for default font size/zoom settings.
   const RECENT_SELECTION_LIMIT = 8 * 1; // at the default font size, there are 8 per row
   const DEFAULT_RESULTS = ['🔮'];
   const WORD_SEPARATORS = /\s+/; // Specifies how search strings are tokenized
