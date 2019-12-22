6bd952ce42945aeabc2d3b0a3164c1ba125164c8
Eric Miller <hi@simple.gy>
2019-12-22 15:22:08 -0800

    fix comment

--- src/js_utils/code_points.test.js
+++ src/js_utils/code_points.test.js
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-const {fromCodePoints, toCodePoints} = require('./code_points'); // re-exporting these for convenience
+const {fromCodePoints, toCodePoints} = require('./code_points');
 
 
 
